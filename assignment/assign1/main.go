2c34f6c9f82ef03c475182696d17fd51693ea47a
Nikita-123456 <124901463+Nikita-123456@users.noreply.github.com>
2023-07-07 16:33:54 +0530

    added

--- assignment/assign1/main.go
+++ assignment/assign1/main.go
@@ -77,7 +77,7 @@ func main() {
 
 	//adding few print statements
 	fmt.Println("hello world")
-	fmt.Println("hello world2")
+	fmt.Println("hello world3")
 
 	fmt.Println("Views on video2 - ", <-c2)
 
